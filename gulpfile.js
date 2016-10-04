558855d1cd0ac2a293f9c8aa3733ee81a91590d2
Nutty Neko <nutty@nuttyneko.com>
2016-10-04 02:58:42 -0700

    add Bootstrap as a submodule
    One of the goals of circle is minimalism. One way to do this,
    is to preprocess only a subset of Bootstrap that will actually
    be used to conserve bandwidth.
    
    So in `scss/bootstrap.scss`, I've essentially only imported
    the grid modules.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass');
+
+gulp.task('styles', () => {
+  gulp.src('./scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default', () => {
+  gulp.start('styles');
+  gulp.watch('./scss/*.scss', ['styles']);
+});
